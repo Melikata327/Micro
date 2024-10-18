97f6bf2488252c03333487b254b6b838a9fed811
Melikata327 <melika.ta2020@gmail.com>
2024-10-18 21:02:00 +0330

    Add files via upload

---
+++ 2.ino.cpp
@@ -0,0 +1,15 @@
+```markdown
+```cpp
+int led1 = 12;
+int led2 = 13;
+void setup () {
+ pinMode(led,OUTPUT);
+}
+void loop(){
+digitalWrite(led1,HIGH);
+delay(1000);
+digitalWrite(led2,LOW);
+delay(1000);
+}
+```
+https://github.com/Melikata327/Micro.git
